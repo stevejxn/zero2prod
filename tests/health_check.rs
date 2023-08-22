ac52a99876e642686de87020e3ec8a3298a361ff
Steve Jackson <steve@s2j.co.uk>
2023-08-22 18:33:24 +0100

    refactor prior to logging and db addition

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -20,10 +20,62 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+
+    // act
+    let body = "name=steve%20jackson&email=steve%40s2j.co.uk";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    // assert
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    // arrange
+    let app_address = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=steve%jackson", "missing the email"),
+        ("email=steve%40s2j.co.uk", "missing the name"),
+        ("", "missing both email and name"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        // act
+        let response = client
+            .post(&format!("{}/subscriptions", &app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            // additional customised error message on test failure
+            "The API did not fail with 400 Bad Request when the payload was {}.",
+            error_message
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind to random port");
     let port = listener.local_addr().unwrap().port();
-    let server = zero2prod::run(listener).expect("Failed to bind address");
+    let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     // launch server as background task
     let _ = tokio::spawn(server);
 
