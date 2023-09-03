f4a159f4b772f95d5a2b201685e8bf56cc6172bb
Steve Jackson <steve@s2j.co.uk>
2023-09-03 05:58:54 +0100

    ch6 - validation

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -53,6 +53,37 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 }
 
 #[tokio::test]
+async fn subscribe_returns_a_400_when_fields_are_present_but_invalid() {
+    // arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=test@example.com", "empty name"),
+        ("name=test&email=", "empty email"),
+        ("name=test&email=this-is-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        // act
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        // assert
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "The API did not return a 400 when payload was {}.",
+            description
+        );
+    }
+}
+
+#[tokio::test]
 async fn subscribe_returns_a_400_when_data_is_missing() {
     // arrange
     let app = spawn_app().await;
