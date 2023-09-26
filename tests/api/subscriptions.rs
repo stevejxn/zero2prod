e85d34ec9ba2818a4616c7a91511df04821f8659
Steve Jackson <steve@s2j.co.uk>
2023-09-26 06:15:24 +0100

    ch7 - error handling

--- tests/api/subscriptions.rs
+++ tests/api/subscriptions.rs
@@ -137,3 +137,22 @@ async fn subscribe_sends_a_confirmation_email_with_a_link() {
 
     assert_eq!(confirmation_links.html, confirmation_links.plain_text);
 }
+
+#[tokio::test]
+async fn subscribe_fails_if_there_is_a_fatal_database_error() {
+    // arrange
+    let app = spawn_app().await;
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+
+    // sabotage the databas
+    sqlx::query!("ALTER TABLE subscriptions DROP COLUMN email;")
+        .execute(&app.db_pool)
+        .await
+        .unwrap();
+
+    // act
+    let response = app.post_subscriptions(body.into()).await;
+
+    // assert
+    assert_eq!(response.status().as_u16(), 500);
+}
